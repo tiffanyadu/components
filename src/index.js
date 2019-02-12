44128409d99d1d978bb5aa063766491a7b03d8f8
tiffanyadu <tadofdesign@gmail.com>
2019-02-11 20:28:29 -0600

    Create Approval Card component and wrap around Comment Detail

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" text="Wow!" avatar={faker.image.avatar()} />
-      <CommentDetail author="Alex" timeAgo="Today at 2:00AM" text="I love this." avatar={faker.image.avatar()} />
-      <CommentDetail author="Jane" timeAgo="Yesterday at 8:10PM" text="That's crazy!" avatar={faker.image.avatar()} />
+      <ApprovalCard>
+        <CommentDetail author="Joe" timeAgo="Today at 4:45PM" text="So cool!" avatar={faker.image.avatar()} />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author="Sam" timeAgo="Today at 4:45PM" text="Wow!" avatar={faker.image.avatar()} />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author="Alex" timeAgo="Today at 2:00AM" text="I love this." avatar={faker.image.avatar()} />
+      </ApprovalCard>
     </div>
   );
 };
