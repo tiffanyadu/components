6e1a421a0f6d22afd3b3ed093245ada74812fa3e
tiffanyadu <tadofdesign@gmail.com>
2019-02-11 19:48:41 -0600

    Pass timeAgo, text, and avatar props

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -5,16 +5,16 @@ const CommentDetail = props => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()} />
+        <img alt="avatar" src={props.avatar} />
       </a>
       <div className="content">
         <a href="/" className="author">
           {props.author}
         </a>
         <div className="metadata">
-          <span className="date">Today at 6:49PM</span>
+          <span className="date">{props.timeAgo}</span>
         </div>
-        <div className="text">Love it!</div>
+        <div className="text">{props.text}</div>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Jane" />
+      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" text="Wow!" avatar={faker.image.avatar()} />
+      <CommentDetail author="Alex" timeAgo="Today at 2:00AM" text="I love this." avatar={faker.image.avatar()} />
+      <CommentDetail author="Jane" timeAgo="Yesterday at 8:10PM" text="That's crazy!" avatar={faker.image.avatar()} />
     </div>
   );
 };
